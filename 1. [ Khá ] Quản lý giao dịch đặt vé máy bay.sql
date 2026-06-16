6aafd78773adae36de912a21d70b46e8d9d6a75b
Nguyễn Mạnh Thắng <thangnm222@uef.edu.vn>
2026-06-16 14:58:09 +0700

    Add files via upload

---
+++ 1. [ Khá ] Quản lý giao dịch đặt vé máy bay.sql
@@ -0,0 +1,49 @@
+CREATE DATABASE airline_booking;
+
+DROP TABLE IF EXISTS bookings;
+DROP TABLE IF EXISTS flights;
+
+CREATE TABLE flights (
+    flight_id SERIAL PRIMARY KEY,
+    flight_name VARCHAR(100),
+    available_seats INT
+);
+
+CREATE TABLE bookings (
+    booking_id SERIAL PRIMARY KEY,
+    flight_id INT REFERENCES flights(flight_id),
+    customer_name VARCHAR(100)
+);
+
+INSERT INTO flights (flight_name, available_seats)
+VALUES ('VN123', 3), ('VN456', 2);
+
+-- Giao dịch thành công
+BEGIN;
+
+UPDATE flights
+SET available_seats = available_seats - 1
+WHERE flight_name = 'VN123';
+
+INSERT INTO bookings (flight_id, customer_name)
+VALUES ((SELECT flight_id FROM flights WHERE flight_name = 'VN123'), 'Nguyen Van A');
+
+COMMIT;
+
+SELECT * FROM flights;
+SELECT * FROM bookings;
+
+-- Giao dịch lỗi và Rollback
+BEGIN;
+
+UPDATE flights
+SET available_seats = available_seats - 1
+WHERE flight_name = 'VN456';
+
+INSERT INTO bookings (flight_id, customer_name)
+VALUES (999, 'Tran Van B');
+
+ROLLBACK;
+
+SELECT * FROM flights;
+SELECT * FROM bookings;
